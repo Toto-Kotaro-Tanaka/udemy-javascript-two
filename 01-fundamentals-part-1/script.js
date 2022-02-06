d09ef2f68c57973564f330dd89158000256f648e
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2022-02-06 12:54:58 +0000

    Operator precedence

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -53,4 +53,43 @@ console.log(job);
 
 lastName = "tanaka"; // This is possible
 console.log(lastName);
+
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); // 2 ** 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmidtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5;
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 19);
+
+const isFullAge = ageSarah >= 18;
 */
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10 <- right to left
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
