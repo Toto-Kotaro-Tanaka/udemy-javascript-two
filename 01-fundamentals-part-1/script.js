bf6aab5bac75cf02cfb6b9baa4f61f15e9933a8a
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2022-02-06 12:21:42 +0000

    Values & variables, data types and let const var

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -1,6 +1,56 @@
+/*
 let js = "amazing";
+
 if (js === "amazing") {
     alert("JS is fun!");
 }
 
 window.console.log(49 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "kotaro";
+console.log(firstName);
+
+let $hero = "Ultraman";
+console.log($hero);
+
+// Describable
+let myFirstJob = "programmer";
+let mySecondJob = "teacher";
+
+// Bad practice
+let job1 = "programme";
+let job2 = "teacher";
+
+true;
+console.log(true);
+let javaScritptIsFun = true;
+javaScritptIsFun = "true"; // Dynamic typing
+console.log(javaScritptIsFun);
+
+console.log(typeof javaScritptIsFun);
+
+let thisIsUndefined; // Declared but not initialised
+console.log(thisIsUndefined);
+console.log(typeof thisIsUndefinded);
+
+thisIsUndefined = "Not undefined anymore";
+console.log(typeof thisIsUndefined);
+
+console.log(typeof null);
+
+let age = 30;
+age = 45; // Reassigning / Mutating
+
+const birthYear = 1991;
+// birthYear = 1992;
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+
+lastName = "tanaka"; // This is possible
+console.log(lastName);
+*/
