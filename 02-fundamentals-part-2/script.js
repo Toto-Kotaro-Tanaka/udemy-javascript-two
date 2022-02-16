12b6c670d72b60d66216739ddb56f430bcfb3b9b
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2022-02-16 20:13:08 +0000

    Functions

--- 02-fundamentals-part-2/script.js
+++ 02-fundamentals-part-2/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -7,3 +7,28 @@ if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive");
 
 // const interface = "Audio"; // Possible reserve word
+*/
+
+function logger() {
+    console.log("My name is Jonas");
+}
+
+logger(); // calling, running, invoking, executing function
+console.log(typeof logger()); // <-- Undefined
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const orangeJuice = fruitProcessor(0, 4);
+console.log(orangeJuice);
+
+const mixJuice = fruitProcessor(3, 6);
+console.log(mixJuice);
