4f7b417c951836848c40dd7797726c421de5f12e
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2022-03-05 20:12:58 +0000

    Event listener

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -7,10 +7,20 @@ console.log(message);
 
 message.innerHTML = "This is innerHTML";
 message.textContent = "This is text content";
-*/
+
 document.querySelector(".message").textContent;
 document.querySelector(".message").textContent = "Correct Number";
 document.querySelector(".number").textContent = "13";
 document.querySelector(".score").textContent = "5";
 document.querySelector(".guess").value = "10"; // value property
 console.log(document.querySelector(".guess").value);
+*/
+
+document.querySelector(".check").addEventListener("click", function() {
+    let guess = Number(document.querySelector(".guess").value);
+    console.log(typeof guess, guess);
+
+    if (!guess) {
+        document.querySelector(".message").textContent = "No number...";
+    }
+});
