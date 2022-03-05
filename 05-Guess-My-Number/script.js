6d5bceb0ed410435c4cee8a0b166e71aff13e2dc
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2022-03-05 19:59:39 +0000

    Selecting and maniplulating elements

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+/* Note
+const message = document.querySelector(".message");
+
+console.log(message);
+
+message.innerHTML = "This is innerHTML";
+message.textContent = "This is text content";
+*/
+document.querySelector(".message").textContent;
+document.querySelector(".message").textContent = "Correct Number";
+document.querySelector(".number").textContent = "13";
+document.querySelector(".score").textContent = "5";
+document.querySelector(".guess").value = "10"; // value property
+console.log(document.querySelector(".guess").value);
