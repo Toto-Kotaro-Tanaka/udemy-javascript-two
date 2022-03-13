6743308d6f7f255704c55e700ce429aef7be50e4
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2022-03-13 13:08:01 +0000

    Implementing game logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -16,11 +16,38 @@ document.querySelector(".guess").value = "10"; // value property
 console.log(document.querySelector(".guess").value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function() {
     let guess = Number(document.querySelector(".guess").value);
     console.log(typeof guess, guess);
 
     if (!guess) {
         document.querySelector(".message").textContent = "No number...";
+    } else if (guess === secretNumber) {
+        document.querySelector(".message").textContent = "You got it correct!";
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "Too high";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent =
+                "You've lost the game...";
+            document.querySelector(".score").textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "Too low";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent =
+                "You've lost the game...";
+            document.querySelector(".score").textContent = 0;
+        }
     }
 });
