b73e52afd0ee27ce6dc0a1cd9658191d35e9b0e0
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2022-03-17 16:16:39 +0000

    Add high score

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -18,7 +18,7 @@ console.log(document.querySelector(".guess").value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-console.log(secretNumber);
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function() {
     let guess = Number(document.querySelector(".guess").value);
@@ -33,6 +33,11 @@ document.querySelector(".check").addEventListener("click", function() {
         document.querySelector(".message").textContent = "You got it correct!";
         document.querySelector("body").style.backgroundColor = "#60b347";
         document.querySelector(".number").style.width = "30rem";
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector(".highscore").textContent = highScore;
+        }
 
         // When number is too high
     } else if (guess > secretNumber) {
